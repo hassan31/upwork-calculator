b00908c1caa372a4097a5c165519b9fb7cf6acc1
Muhammad Hassan <>
2025-03-15 18:34:37 -0400

    Upwork Calculator

---
+++ CalculatorView/CalculatorView/CalculatorViewApp.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorViewApp.swift
+//  CalculatorView
+//
+//  Created by Muhammad Hassan on 2025-03-15.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalculatorView()
+                .frame(minWidth: 500, minHeight: 800)
+        }
+    }
+}
---
+++ CalculatorView/CalculatorView/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  CalculatorView
+//
+//  Created by Muhammad Hassan on 2025-03-15.
+//
+
+import SwiftUI
+
+extension Color {
+    static var platformBackground: Color {
+        #if os(iOS)
+        return Color(.systemGray6)
+        #else
+        return Color.gray.opacity(0.2) // macOS alternative
+        #endif
+    }
+}
